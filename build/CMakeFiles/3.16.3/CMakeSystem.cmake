52526471915935fe9c93305bd4af3b359a450261
khanafaq0099 <khanafaq526@gmail.com>
2023-10-06 15:29:08 +0500

    fixed

--- build/CMakeFiles/3.16.3/CMakeSystem.cmake
+++ build/CMakeFiles/3.16.3/CMakeSystem.cmake
@@ -1,6 +1,6 @@
-set(CMAKE_HOST_SYSTEM "Linux-5.15.0-83-generic")
+set(CMAKE_HOST_SYSTEM "Linux-5.15.0-84-generic")
 set(CMAKE_HOST_SYSTEM_NAME "Linux")
-set(CMAKE_HOST_SYSTEM_VERSION "5.15.0-83-generic")
+set(CMAKE_HOST_SYSTEM_VERSION "5.15.0-84-generic")
 set(CMAKE_HOST_SYSTEM_PROCESSOR "x86_64")
 
 include("/home/afaq/esp/esp-idf/tools/cmake/toolchain-esp32.cmake")
