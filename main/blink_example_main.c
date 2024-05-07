86f65b4fd724258640ebf1eacf20fbbff01c2a66
khanafaq0099 <khanafaq526@gmail.com>
2024-05-07 11:45:54 +0500

    led pin change to 13

--- main/blink_example_main.c
+++ main/blink_example_main.c
@@ -9,16 +9,11 @@
 #include "esp_http_client.h"
 #include "esp_log.h"
 
-
-
 static const char *TAG = "BLINK";
 
 extern const uint8_t server_cert_pem_start[] asm("_binary_ca_cert_pem_start");
 extern const uint8_t server_cert_pem_end[] asm("_binary_ca_cert_pem_end");
-// #define BLINK_GPIO CONFIG_BLINK_GPIO // 5
-#define led_pin 2
-
-
+#define led_pin 13
 
 void app_main(void)
 {
@@ -32,62 +27,3 @@ void app_main(void)
         vTaskDelay(1000 / portTICK_PERIOD_MS);
     }
 }
-
-
-// void app_main(void)
-// {
-//     gpio_set_direction(button_pin, GPIO_MODE_INPUT);
-
-//     while (1)
-//     {
-//         uint8_t s_led_state = gpio_get_level(button_pin);
-//         if (s_led_state)
-//         {
-//             printf("led on----------------------------------->");
-//         }
-//         else
-//         {
-//             printf("led offff----------------------------------->");
-//         }
-//     }
-// }
-
-// void app_main(void)
-// {
-//     esp_http_client_config_t config = {
-//         .url = "https://parseapi.back4app.com/classes/B4aVehicle/jadkSa4E7W",
-//         .method = HTTP_METHOD_PUT,
-//         .headers = {
-//             "X-Parse-Application-Id" : "xc6ma9Ot8MP7HfVJ0QiojPExB8paEcIqBT5abnCB",
-//             "X-Parse-REST-API-Key" : "z2jm18Wsdiv3B0jJMSmgk5OD6XN0icMsNJghy4nO",
-//             "Content-Type" : "application/json",
-//         },
-//         .buffer_size = 1024,
-//     };
-
-//     esp_http_client_config_t config = {
-//         .url = "https://parseapi.back4app.com/classes/B4aVehicle/jadkSa4E7W",
-//     };
-
-//     esp_http_client_handle_t client = esp_http_client_init(&config);
-//      esp_http_client_set_header(client, "xc6ma9Ot8MP7HfVJ0QiojPExB8paEcIqBT5abnCB", "z2jm18Wsdiv3B0jJMSmgk5OD6XN0icMsNJghy4nO");
-
-//     esp_http_client_set_method(client, HTTP_METHOD_PUT);
-
-//     char post_data[100];
-//     //    = "{\"state\": \"on\"}";
-//     sprintf(post_data, "{\"state\": \"%d\"}", s_led_state);
-//     // esp_http_client_handle_t client = esp_http_client_init(&config);
-//     esp_http_client_set_post_field(client, post_data, strlen(post_data));
-//     esp_err_t err = esp_http_client_perform(client);
-//     esp_http_client_cleanup(client);
-
-//     if (err == ESP_OK)
-//     {
-//         printf("Data sent successfully to Back4App\n");
-//     }
-//     else
-//     {
-//         printf("Failed to send data to Back4App. Error code: %d\n", err);
-//     }
-// }
